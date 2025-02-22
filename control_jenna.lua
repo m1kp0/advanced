2187d37f854bb718a5ee8b253b302a278aba50eb
m1kpe0 <yarikasik@inbox.ru>
2025-02-23 00:45:20 +0300

    Create control_jenna.lua

---
+++ control_jenna.lua
@@ -0,0 +1,19 @@
+local p = game.Players.LocalPlayer
+local c = p.Character
+local hrp = c.HumanoidRootPart
+local h = c.Humanoid
+local rs = game:GetService("RunService")
+
+rs.Heartbeat:Connect(function()
+  for i, v in pairs(workspace.killers:GetChildren()) do
+    if v.Name == 'Jenna' then
+      for i, n in pairs(v:GetChildren()) do
+        if n.Name == 'Jenna' then
+			if not h.Health == 0 then
+          		n.Head.Handle.CFrame = hrp.CFrame * CFrame.new(0, 5, 10)
+			end
+        end
+      end
+    end
+  end
+end)
