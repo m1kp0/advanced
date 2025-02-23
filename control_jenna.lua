3e55f831ddfff1a4dc351c52a9f25c2a36a80d44
m1kpe0 <yarikasik@inbox.ru>
2025-02-23 21:15:37 +0300

    Update control_jenna.lua

--- control_jenna.lua
+++ control_jenna.lua
@@ -5,15 +5,15 @@ local h = c.Humanoid
 local rs = game:GetService("RunService")
 
 rs.Heartbeat:Connect(function()
-  for i, v in pairs(workspace.killers:GetChildren()) do
-    if v.Name == 'Jenna' then
-      for i, n in pairs(v:GetChildren()) do
-        if n.Name == 'Jenna' then
-			if h.Health ~= 0 then
-          		n.Head.Handle.CFrame = hrp.CFrame * CFrame.new(0, 5, 10)
+	for i, v in pairs(workspace.killers:GetChildren()) do
+		if v.Name == 'Jenna' then
+			for i, n in pairs(v:GetChildren()) do
+				if n.Name == 'Jenna' then
+					if h.Health ~= 0 then
+						n.Head.Handle.CFrame = hrp.CFrame * CFrame.new(0, 5, 10)
+					end
+				end
 			end
-        end
-      end
-    end
-  end
+		end
+	end
 end)
